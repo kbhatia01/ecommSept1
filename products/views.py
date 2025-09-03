587f6760eeb03a18e6ae9e49300be31aee58c298
karan.bhatia <karanbhatiacs@gmail.com>
2025-09-03 22:41:18 +0530

    models admin

--- products/views.py
+++ products/views.py
@@ -4,10 +4,15 @@
 
 from django.http import JsonResponse
 
+from products.models import Product
+
+
 def HelloView(request):
     print(request.headers)
     return JsonResponse({'hello': 'NAME'})
 
 
 def HelloName(request, username):
+    p = Product.objects.all()
+    print(p[1].__dict__)
     return JsonResponse({'hello': username})
