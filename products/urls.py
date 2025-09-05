3d5ade4581f6266786f0bad2b0a1d63b279aa3d9
karan.bhatia <karanbhatiacs@gmail.com>
2025-09-05 22:31:10 +0530

    serializer

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from products import views
+from products.views import createProduct
 
 urlpatterns = [
     path("hello/", views.HelloView),
-    path("hello/<slug:username>", views.HelloName)
+    path("hello/<slug:username>", views.HelloName),
+    path('createProduct/', createProduct)
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,13 @@
-
+import json
 # Create your views here.
 
 
 from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from products.create_product_serializer import createProductSerializer
 from products.models import Product
 
 
@@ -16,3 +20,13 @@ def HelloName(request, username):
     p = Product.objects.all()
     print(p[1].__dict__)
     return JsonResponse({'hello': username})
+
+@api_view(['POST'])
+def createProduct(request):
+    serlizedData = createProductSerializer(data=request.data)
+    print("a", serlizedData)
+    if serlizedData.is_valid():
+        prod = serlizedData.save()
+        return Response(createProductSerializer(prod).data, status=status.HTTP_201_CREATED)
+
+    return JsonResponse({'Error': serlizedData.errors})
